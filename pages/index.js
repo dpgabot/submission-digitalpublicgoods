4d86c92ae44b9bd1a9016a29c5028656712d213b
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-10-12 12:14:04 -0600

    BLD: initial fields + validation

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,100 @@
 import Head from 'next/head'
+import FormRenderer from '@data-driven-forms/react-form-renderer';
+import { FormTemplate, componentMapper } from '@data-driven-forms/mui-component-mapper';
+
+import Box from '@material-ui/core/Box';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+import schema from './schema';
+
+const validatorMapper = {}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  }
+
+}));
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiButton: {
+      root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+    MuiCheckbox: {
+      root: {
+        padding: '1px 9px'
+      }
+    }
+  },
+});
+
+const validate = (values) => {
+  console.log(values)
+  const errors = {};
+
+  for(let i=1; i<=17; i++){
+    if (values.SDGs && values.SDGs.includes(i) && ! values['evidenceText'+i] && ! values['evidenceURL'+i]) {
+      errors['evidenceText'+i] = 'Either the description or a URL is required';
+      errors['evidenceURL'+i] = 'Either the description or a URL is required';
+    }
+  }
+
+  return errors;
+}
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
-    <div className="container">
+    <Container component="main" maxWidth="sm">
+
       <Head>
-        <title>Create Next App</title>
+        <title>Submission Form</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className={classes.paper}>
+        <ThemeProvider theme={theme}>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+          <FormRenderer 
+            validate={validate}
+            schema={schema}
+            onSubmit={(values, formApi) => console.log(values)}
+            FormTemplate={FormTemplate}
+            componentMapper={componentMapper}
+            validatorMapper={validatorMapper} // not required
+          />
+        </ThemeProvider>
+      </div>
+      
+    </Container>
   )
 }
---
+++ pages/schema.js
@@ -0,0 +1,198 @@
+import validatorTypes from '@data-driven-forms/react-form-renderer/dist/cjs/validator-types';
+
+const schema = {
+    title: 'Digital Public Goods Submission',
+    description: `This is a BETA version of our submission form and process which means we'll be learning from it and may contact you for follow-up information and input on the process as well as your project submission.`,
+    fields: [{
+            name: 'name',
+            component: 'text-field',
+            label: 'Project name',
+            helperText: 'For example, Wikipedia.',
+            validate: [{
+                type: 'required'
+            }]
+        },
+        {
+            name: 'description',
+            component: 'text-field',
+            label: 'Project Description',
+            helperText: 'Include a concise 1-line description for this project.',
+            validate: [{
+                    type: 'required'
+            }]
+        },
+        {
+            name: 'website',
+            component: 'text-field',
+            label: 'Project Website URL',
+            helperText: 'Public website (string must include http(s)://)',
+            validate: [{
+                    type: validatorTypes.URL
+                }
+            ],
+        },
+        {
+            name: 'type',
+            component: 'checkbox',
+            label: 'What category best describes this project?',
+            helperText: 'Select all that apply.',
+            options: [
+            	{
+            		label: "Open Source Software",
+            		value: "software"
+            	},
+            	{
+            		label: "Open Data",
+            		value: "data"
+            	},
+            	{
+            		label: "Open Content",
+            		value: "content"
+            	},
+            	{
+            		label: "Open Artificial Intelligence (AI) Model",
+            		value: "model"
+            	},
+            	{
+            		label: "Open Standard",
+            		value: "standard"
+            	}
+            ],
+            validate: [{
+                type: 'required'
+            }]
+        },
+        {
+            name: 'repositoryURL',
+            component: 'text-field',
+            label: 'Link to Github (or other) repository',
+            helperText: 'Required for open source software - link to public repository',
+            condition: {
+            	when: 'type',
+            	pattern: /software/
+            },
+            validate: [{
+                    type: 'required'
+                }
+            ],
+            classes: {root: 'conditional'},
+
+        },
+        {
+            name: 'SDGs',
+            component: 'checkbox',
+            label: 'Please identify which of the Sustainable Development Goals this project is relevant to',
+            helperText: 'Select all that apply.',
+            options: [
+            	{
+            		label: "1: End Poverty in all its forms everywhere",
+            		value: 1
+            	},
+            	{
+            		label: "2: Zero Hunger",
+            		value: 2
+            	},
+            	{
+            		label: "3: Good Health and Well-Being",
+            		value: 3
+            	},
+            	{
+            		label: "4: Quality Education",
+            		value: 4
+            	},
+            	{
+            		label: "5: Gender Equity",
+            		value: 5
+            	},
+            	{
+            		label: "6: Clean Water and Sanitation",
+            		value: 6
+            	},
+            	{
+            		label: "7: Affordable and Clean Energy",
+            		value: 7
+            	},
+            	{
+            		label: "8: Decent Work and Economic Growth",
+            		value: 8
+            	},
+            	{
+            		label: "9: Industry, Innovation and Infrastructure",
+            		value: 9
+            	},
+            	{
+            		label: "10: Reduced Inequalities",
+            		value: 10
+            	},
+            	{
+            		label: "11: Sustainable Cities and Communities",
+            		value: 11
+            	},
+            	{
+            		label: "12: Responsible Consumption and Production",
+            		value: 12
+            	},
+            	{
+            		label: "13: Climate Action",
+            		value: 13
+            	},
+            	{
+            		label: "14: Life Below Water",
+            		value: 14
+            	},
+            	{
+            		label: "15: Life on Land",
+            		value: 15
+            	},
+            	{
+            		label: "16: Peace, Justice and Strong Institutions",
+            		value: 16
+            	},
+            	{
+            		label: "17: Partnerships for the Goals",
+            		value: 17
+            	}
+            ],
+            validate: [{
+                type: 'required'
+            }]
+        },
+    ]
+};
+
+// Dynamically add a subform for each SDG that only display conditionally if the 
+// corresponding SDG checkbox is checked. Validation for these fields is coded in React, not here.
+for(let i=1; i<=17; i++) {
+    schema.fields.push(
+        {
+            name: 'subform'+i,
+            component: 'sub-form',
+            description: 'Please supply information or link to support relevance to SDG '+i+':',
+            condition: {
+                when: 'SDGs',
+                pattern: new RegExp('\\b'+i+'\\b')
+            },
+            classes: {root: 'conditional'},
+            fields: [
+                {
+                    name: 'evidenceText'+i,
+                    component: 'text-field',
+                    label: 'Description:',
+                    validate: [{
+                        type: 'required'
+                    }]              
+                },
+                {
+                    name: 'evidenceURL'+i,
+                    component: 'text-field',
+                    label: 'URL:',
+                    validate: [{
+                        type: validatorTypes.URL
+                    }],
+                }
+            ]
+        }
+    )
+}
+
+export default schema;

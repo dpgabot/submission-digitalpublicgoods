42cabaf2af55bf19a4c1c673ef11b954cadb6a59
vaishnavi-ch <66664653+vaishnavi-ch@users.noreply.github.com>
2021-04-27 10:33:53 +0530

    Update schema.js

--- schemas/schema.js
+++ schemas/schema.js
@@ -262,6 +262,13 @@ const schema = {
                   name: "organizations[contact_email]",
                   component: "text-field",
                   label: "Contact email",
+                  validate: [
+                    {
+                      type: validatorTypes.PATTERN,
+                      pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$',
+                      message: 'Not valid email',
+                    },
+                  ],
                 },
               ],
             },

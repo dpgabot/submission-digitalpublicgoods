a02e6323fa85b79fad354183e64be1fd38326905
nathanbaleeta <nbaleeta@gmail.com>
2021-04-29 16:58:36 +0300

    [BUG] nominations & full DPG submissions keep alternating between  distorted and valid on subsequent pull request submissions

--- pages/api/openPR.js
+++ pages/api/openPR.js
@@ -15,6 +15,11 @@ const GITHUB_BRANCH = process.env.GITHUB_BRANCH
   ? process.env.GITHUB_BRANCH
   : "main"; /* optional: defaults to default branch */
 
+// Return Project name
+function getProjectName(values) {
+  return values.name;
+}
+
 // Parse name of project prior to saving file
 function parseProjectName(values) {
   return (
@@ -24,59 +29,6 @@ function parseProjectName(values) {
       .replace(/[\u0300-\u036f]/g, "")
       .replace(/ /g, "-") + ".json"
   );
-}
-
-// Return multiple submission files including corresponding filepaths
-function getSubmissionFiles(values, myJSON) {
-  let name,
-    nomineePath,
-    dpgPath,
-    files = {};
-
-  let dpgJSON = JSON.stringify(myJSON, null, 3);
-
-  // Add newline at end of file
-  dpgJSON += "\r\n";
-
-  // Parse project names using DPG naming convention for filenames
-  name = parseProjectName(values);
-
-  // Add nominee submission to nominee directory
-  nomineePath = "nominees/" + `${name}`;
-
-  // Add DPG submission to screening directory
-  dpgPath = "screening/" + `${name}`;
-
-  values.stage === "nominee"
-    ? Object.entries(myJSON).forEach(
-        ([key, value]) =>
-          (files = {
-            [nomineePath]: {
-              content: myJSON,
-              encoding: "utf-8",
-            },
-          })
-      )
-    : Object.entries(dpgJSON).forEach(
-        ([key, value]) =>
-          (files = {
-            [nomineePath]: {
-              content: dpgJSON[0],
-              encoding: "utf-8",
-            },
-            [dpgPath]: {
-              content: dpgJSON[1],
-              encoding: "utf-8",
-            },
-          })
-      );
-
-  return files;
-}
-
-// Return Project name
-function getProjectName(values) {
-  return values.name;
 }
 
 // Create Github checkout branch for current submissions
@@ -105,12 +57,74 @@ function getSDGRelevanceInfo(values, sdgNumber, evidenceText) {
   return values;
 }
 
+// Return multiple submission files including corresponding filepaths
+function getSubmissionFiles(values, myJSON) {
+  let name,
+    nomineePath,
+    dpgPath,
+    files = {};
+
+  // Delete multiple DPG nomination fields
+  [
+    "aliases",
+    "description",
+    "website",
+    "type",
+    "SDGs",
+    "license",
+    "organizations",
+    "stage",
+    "repositoryURL",
+  ].forEach((e) => delete values[e]);
+
+  // Convert JavaScript submission sorted object into JSON string and add newline at EOF
+  let data = JSON.stringify(values, null, 3).concat("\n");
+
+  // Convert string into Javascript object then delete unnecessary keys from object
+  //myJSON = JSON.parse(myJSON);
+
+  // Parse project names using DPG naming convention for filenames
+  name = parseProjectName(values);
+
+  // Add nominee submission to nominee directory
+  nomineePath = "nominees/" + `${name}`;
+
+  // Add DPG submission to screening directory
+  dpgPath = "screening/" + `${name}`;
+
+  values.stage === "nominee"
+    ? Object.entries(myJSON).forEach(
+        ([key, value]) =>
+          (files = {
+            [nomineePath]: {
+              content: myJSON,
+              encoding: "utf-8",
+            },
+          })
+      )
+    : Object.entries(data).forEach(
+        ([key, value]) =>
+          (files = {
+            [nomineePath]: {
+              content: myJSON,
+              encoding: "utf-8",
+            },
+            [dpgPath]: {
+              content: data,
+              encoding: "utf-8",
+            },
+          })
+      );
+
+  return files;
+}
+
 // Nominee processing before opening pull request
-function nomineeSubmission(values, sortedNomineeSubmission) {
+function nomineeSubmission(values, sortedSubmission) {
   // Sort entries by iterating through object and unpacking entries
   Object.entries(values).forEach(
     ([key, value]) =>
-      (sortedNomineeSubmission = {
+      (sortedSubmission = {
         name: values.name ? values.name : "",
         aliases: values.aliases ? [values.aliases] : [""],
         description: values.description ? values.description : "",
@@ -125,37 +139,28 @@ function nomineeSubmission(values, sortedNomineeSubmission) {
       })
   );
 
-  console.log(sortedNomineeSubmission);
-  return sortedNomineeSubmission;
+  console.log(sortedSubmission);
+  return sortedSubmission;
 }
 
 // DPG candidate processing before opening pull request
-function dpgSubmission(
-  values,
-  sortedNomineeSubmission,
-  sortedDPGSubmission,
-  sortedSubmission
-) {
-  sortedNomineeSubmission = nomineeSubmission(values, sortedNomineeSubmission);
-  // Delete multiple DPG nomination fields
-  [
-    "aliases",
-    "description",
-    "website",
-    "type",
-    "SDGs",
-    "license",
-    "organizations",
-    "stage",
-    "repositoryURL",
-  ].forEach((e) => delete values[e]);
-
+function dpgSubmission(values, sortedSubmission) {
   // Order fields by iterating through object
   Object.entries(values).forEach(
     ([key, value]) =>
-      (sortedDPGSubmission = {
+      (sortedSubmission = {
         name: values.name ? values.name : "",
-        clearOwnership: values.clearOwnership ? values.clearOwnership : "",
+        aliases: values.aliases ? [values.aliases] : [""],
+        description: values.description ? values.description : "",
+        website: values.website ? values.website : "",
+        license: values.license ? values.license : [],
+        SDGs: values.SDGs ? values.SDGs : [],
+        sectors: values.sectors ? values.sectors : [],
+        type: values.type ? values.type : [],
+        repositoryURL: values.repositoryURL ? values.repositoryURL : "",
+        organizations: values.organizations ? values.organizations : [],
+        stage: values.stage ? values.stage : "",
+        /* clearOwnership: values.clearOwnership ? values.clearOwnership : "",
         platformIndependence: values.platformIndependence
           ? values.platformIndependence
           : "",
@@ -164,18 +169,9 @@ function dpgSubmission(
         privacy: values.privacy ? values.privacy : "",
         standards: values.standards ? values.standards : "",
         doNoHarm: values.doNoHarm ? values.doNoHarm : "",
-        locations: values.locations ? values.locations : {},
+        locations: values.locations ? values.locations : {}, */
       })
   );
-
-  console.log(sortedNomineeSubmission);
-  console.log("###################################################");
-  console.log(sortedDPGSubmission);
-
-  sortedSubmission = [sortedNomineeSubmission, sortedDPGSubmission];
-
-  console.log("###################################################");
-  console.log(sortedSubmission);
 
   return sortedSubmission;
 }
@@ -184,13 +180,7 @@ export default async (req, res) => {
   if (req.method === "POST") {
     let values = req.body.values;
 
-    let myJSON,
-      name,
-      sdgNumber,
-      evidenceText,
-      sortedNomineeSubmission,
-      sortedDPGSubmission,
-      sortedSubmission;
+    let myJSON, name, sdgNumber, evidenceText, sortedSubmission;
 
     // Exclude contact information from pull request
     delete values["contact"];
@@ -202,20 +192,11 @@ export default async (req, res) => {
 
     sortedSubmission =
       values.stage === "nominee"
-        ? nomineeSubmission(values, sortedNomineeSubmission)
-        : dpgSubmission(
-            values,
-            sortedNomineeSubmission,
-            sortedDPGSubmission,
-            sortedSubmission
-          );
+        ? nomineeSubmission(values, sortedSubmission)
+        : nomineeSubmission(values, sortedSubmission);
 
-    // Convert JavaScript submission sorted object into JSON string
-
-    myJSON = JSON.stringify(sortedSubmission, null, 3);
-
-    // Add newline at end of file
-    myJSON += "\r\n";
+    // Convert JavaScript submission sorted object into JSON string and add newline at EOF
+    myJSON = JSON.stringify(sortedSubmission, null, 3).concat("\n");
 
     const MyOctokit = Octokit.plugin(createPullRequest);
 

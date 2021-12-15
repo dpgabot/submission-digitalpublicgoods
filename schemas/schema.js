76f5f77ed7ca42d76ed4478aa15acc86779c8270
Nathan Kwabena Fletcher <fletcher.nathan.a@gmail.com>
2021-12-15 11:55:07 +0000

    updated email validation regex

--- schemas/schema.js
+++ schemas/schema.js
@@ -88,7 +88,7 @@ const schema = {
                 },
                 {
                   type: "pattern",
-                  pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$",
+                  pattern: "^[w-.]+@([w-]+.)+[w-]{2,4}$",
                   message: "Not valid email",
                 },
               ],
